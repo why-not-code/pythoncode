36be9d50bd3d9f00ccaac80790f126a7f6990408
why-not-code <itssagar@hotmail.com>
2025-07-21 22:46:05 -0500

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -8,6 +8,15 @@ def collatz(input_num):
             input_num = 3 * input_num + 1
     print (1, end='')
 
-input_num = int(input('Enter a positive number: '))
+while True:
+    try:
+        input_num = int(input('Enter a positive number: '))
+        if input_num <= 0:
+            print('please enter a positive integer greater than 0.')
+            continue
+        break
+
+    except ValueError:
+        print ('Invalid input: Please enter a valid integer')
 print('Collatz Sequence', end= ' ')
 collatz(input_num)
